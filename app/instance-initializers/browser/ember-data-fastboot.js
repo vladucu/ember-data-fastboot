02c96c0ca51ffba3281891ef008f14985e7545b9
Vlad Vanca <vanca.vlad@gmail.com>
2017-05-11 13:08:15 +0200

    Fix edge case with empty store

--- app/instance-initializers/browser/ember-data-fastboot.js
+++ app/instance-initializers/browser/ember-data-fastboot.js
@@ -2,9 +2,14 @@ import Ember from 'ember';
 
 export function initialize(applicationInstance) {
   let shoebox = applicationInstance.lookup('service:fastboot').get('shoebox');
-  if (!shoebox) { return; }
+  if (!shoebox) {
+    return;
+  }
+
   let dump = shoebox.retrieve('ember-data-store');
-  if (!dump) { return; }
+  if (!dump || (Object.keys(dump).length === 0)) {
+    return;
+  }
   let store = applicationInstance.lookup('service:store');
 
   Object.keys(dump.records).forEach((modelName) => {
